d4d279f1cc947e327334b98f12b85a596b772f2d
ArpanChoudhuri <arpan.choudhuri@gmail.com>
2025-10-19 13:22:00 -0400

    optimized api/ test fixes

--- Api.Test/UnitTest1.cs
+++ Api.Test/UnitTest1.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
-using Xunit;
+using FluentAssertions;
 
 public class AuthTests : IClassFixture<WebApplicationFactory<Program>>
 {
@@ -15,5 +15,21 @@ public class AuthTests : IClassFixture<WebApplicationFactory<Program>>
         var client = _factory.CreateClient();
         var res = await client.GetAsync("/orders");
         Assert.Equal(HttpStatusCode.Unauthorized, res.StatusCode);
+    }
+}
+
+public class ProductsTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _f;
+    public ProductsTests(WebApplicationFactory<Program> f) => _f = f;
+
+    [Fact]
+    public async Task List_Returns_Ok_And_Paged()
+    {
+        var c = _f.CreateClient();
+        var res = await c.GetAsync("/products?take=2");
+        res.StatusCode.Should().Be(HttpStatusCode.OK);
+        var body = await res.Content.ReadAsStringAsync();
+        body.Should().Contain("items").And.Contain("nextAfter");
     }
 }
