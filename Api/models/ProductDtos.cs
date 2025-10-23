d473b3cf2cb93d3d5f4bd3c7f6100523ea66b311
ArpanChoudhuri <arpan.choudhuri@gmail.com>
2025-10-23 19:50:00 -0400

    arch

--- Api/models/ProductDtos.cs
+++ Api/models/ProductDtos.cs
@@ -4,12 +4,7 @@
     {
         public record ProductListItem(int Id, string Name, decimal Price, Byte[]? RowVersion)
         {
-            public ProductListItem(int id, string name, decimal price)
-            {
-                Id = id;
-                Name = name;
-                Price = price;
-            }
+
         }
     }
 }
